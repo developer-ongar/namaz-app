97917655e064a3084e369b30549f54352e6c1a58
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-01-22 23:28:15 +0600

    Create routes.dart

---
+++ lib/core/constants/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String homePage = '/';
+  static const String taharatPage = 'taharat';
+  static const String namazPage = 'namaz';
+  static const String genderPage = 'gender';
+  static const String namazListPage = 'namaz-list';
+  static const String namazAboutPage = 'namaz-about';
+  static const String faqPage = 'faq';
+}
