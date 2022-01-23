6184990da13b72b8563d61abcadbded85e036ade
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-01-23 16:31:25 +0600

    Create section.model.dart

---
+++ lib/features/home/data/models/section.model.dart
@@ -0,0 +1,13 @@
+class SectionModel {
+  final String title;
+  final String image;
+  final String description;
+  final String route;
+
+  SectionModel({
+    this.title,
+    this.image,
+    this.description,
+    this.route,
+  });
+}
